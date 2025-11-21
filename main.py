fb92438f4f0d6df7972683711b8134504c9ff2b1
ushel <utkarsh.shelke03@gmail.com>
2025-11-21 20:36:15 +0530

    ollama model used

--- main.py
+++ main.py
@@ -1,15 +1,61 @@
 import os
 
 from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+
+# List of messages 1. System message, 2. Human message, 3. AI message
+
+# Langchain workflow user query -> Prompt template (format query into structured prompt) -> language model (generate response) -> Output parser (parse LLM output into structured data)
+
+# -> External API Tool Call (call external service) -> Final LLM call (Process API response) -> Final output
 
 load_dotenv()
+
 
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 
 
 def main():
     print("Hello from langchain!")
-    print(os.environ["OPENAI_API_KEY"])
+    # print(os.environ["OPENAI_API_KEY"])
+
+    information = """
+    
+    Elon Reeve Musk (born June 28, 1971) is a businessman and entrepreneur known for his leadership of Tesla, SpaceX, Twitter, and xAI. Musk has been the wealthiest person in the world since 2021; as of October 2025, Forbes estimates his net worth to be around $500 billion.
+
+    Born into a wealthy family in Pretoria, South Africa, Musk emigrated in 1989 to Canada; his Canadian citizenship is congenital, his mother having been born there. He received bachelor's degrees in 1997 from the University of Pennsylvania in Philadelphia, United States, before moving to California to pursue business ventures. In 1995, Musk co-founded the software company Zip2. Following its sale in 1999, he co-founded X.com, an online payment company that later merged to form PayPal, which was acquired by eBay in 2002. That year, Musk also became an American citizen.
+
+    In 2002, Musk founded the space technology company SpaceX, becoming its CEO and chief engineer; the company has since led innovations in reusable rockets and commercial spaceflight. Musk joined the automaker Tesla as an early investor in 2004 and became its CEO and product architect in 2008; it has since become a leader in electric vehicles. In 2015, he co-founded OpenAI to advance artificial intelligence (AI) research, but later left; growing discontent with the organization's direction and their leadership in the AI boom in the 2020s led him to establish xAI. In 2022, he acquired the social network Twitter, implementing significant changes, and rebranding it as X in 2023. His other businesses include the neurotechnology company Neuralink, which he co-founded in 2016, and the tunneling company the Boring Company, which he founded in 2017. In November 2025, a Tesla pay package worth $1 trillion for Musk was approved, which he is to receive over 10 years if he meets specific goals.
+
+    Musk was the largest donor in the 2024 U.S. presidential election, where he supported Donald Trump. After Trump was inaugurated as president in early 2025, Musk served as Senior Advisor to the President and as the de facto head of DOGE. After a public feud with Trump, Musk left the Trump administration and returned to managing his companies.
+
+    Musk is a supporter of global far-right figures, causes, and political parties. His political activities, views, and statements have made him a polarizing figure. Musk has been criticized for COVID-19 misinformation, promoting conspiracy theories, and affirming antisemitic, racist, and transphobic comments. His acquisition of Twitter was controversial due to a subsequent increase in hate speech and the spread of misinformation on the service. His role in the second Trump administration attracted considerable public backlash, particularly in response to DOGE.
+
+    """
+
+    # important as it will be help in prompt injection as its very much strict structure.
+
+    summary_template = """
+    given the information {information} about a person I want you to create:
+    1. A short summary
+    2. two interesting facts about them
+    """
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
+
+    llm = ChatOpenAI(
+        temperature=0, model="gpt-5"
+    )  # temperature = 0.0-0.3 deterministic, factual and repeatable and good for summarization, code, instruction or test
+    # temperature = 0.8-1.0  give us very creative results good for poetry, fiction, out-of-the box ideas
+
+    chain = (
+        summary_prompt_template | llm
+    )  # | operator will create a runnable chain by connecting the output of left component as an input to right component
+    response = chain.invoke(input={"information": information})
+    print(response.content)
 
 
 if __name__ == "__main__":
