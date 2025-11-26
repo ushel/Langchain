319771d609eaa4fa07cdb47ab360400ea835495d
ushel <utkarsh.shelke03@gmail.com>
2025-11-26 21:25:51 +0530

    langchain-tavily build in tool used

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 WE are using langchain-tavily 
+
+when we trace using langsmith we can see multiple function calling because gpt-5 supports multiple function calling 
+
+final request to llm consist of all the tools calls an their answers pass to the llm to get the final answer.
--- main.py
+++ main.py
@@ -3,34 +3,36 @@ from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
-from tavily import TavilyClient
+# from tavily import TavilyClient
+from langchain_tavily import TavilySearch   # using langchain tool
 
 load_dotenv()
 
-tavily = TavilyClient()
+# tavily = TavilyClient()
 
 
+#using custom tool with help of tavily
+# @tool
+# def search(query: str) -> str:
+#     """
+#     Tool that searches over internet
 
-@tool
-def search(query: str) -> str:
-    """
-    Tool that searches over internt
+#     Args:
+#         query: The query to search for
 
-    Args:
-        query: The query to search for
+#     Returns:
+#         The search result
+#     """
+#     print(f"Searching for {query}")
+#     return tavily.search(query=query)
 
-    Returns:
-        The search result
-    """
-    print(f"Searching for {query}")
-    return tavily.search(query=query)
-
-llm = ChatOpenAI()
-tools = [search]
+llm = ChatOpenAI(model="gpt-5")
+# tools = [search]
+tools = [TavilySearch]
 agent = create_agent(model = llm, tools=tools)
 def main():
     print("Hello from langchain-project!")
-    result = agent.invoke({"messages":HumanMessage(content="What is the weather in tokyo")})
+    result = agent.invoke({"messages":HumanMessage(content="search for 3 job positng for an ai engineer in langchain in the bay area on linkedin and list their details")})
     print(result)
 
 
